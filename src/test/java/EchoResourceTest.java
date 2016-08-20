0ea81270d4d46aca3daed1896587daa986951b43
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2016-08-20 02:09:32 -0400

    init unit tests

---
+++ src/test/java/EchoResourceTest.java
@@ -0,0 +1,18 @@
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gnut3ll4 on 8/20/16.
+ */
+public class EchoResourceTest {
+
+    @Test
+    public void echo() throws Exception {
+        assert false;
+    }
+
+}
---
+++ src/test/java/HelloWorldResourceTest.java
@@ -0,0 +1,14 @@
+
+
+import org.junit.Test;
+
+/**
+ * Created by gnut3ll4 on 8/20/16.
+ */
+public class HelloWorldResourceTest {
+    @Test
+    public void sayhello() throws Exception {
+        assert true;
+    }
+
+}
